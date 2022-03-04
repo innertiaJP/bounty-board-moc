a629d0059139807c8b38d14b65b006c55e902591
innertiaJP <nft.toushi@gmail.com>
2022-03-04 18:40:03 +0900

    create environment

---
+++ src/webfront/.eslintrc.js
@@ -0,0 +1,67 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    "jest/globals": true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    "plugin:@typescript-eslint/recommended",
+    "prettier"
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    "jest",
+    '@typescript-eslint',
+    "prettier",
+  ],
+  rules: {
+    "no-use-before-define": "off",
+    "@typescript-eslint/no-use-before-define": ["error"],
+    "prettier/prettier": "error",
+    '@typescript-eslint/indent': [
+      'error',
+      2,
+    ],
+    '@typescript-eslint/prefer-interface': 'off',
+    'react/jsx-filename-extension': [
+      'error',
+      { extensions: ['.jsx', '.tsx'] },
+     ],
+     'react/prop-types': 'off',
+     'spaced-comment': [
+        'error',
+        'always',
+        { markers: ['/ <reference'] },
+      ],
+      "import/extensions": [
+        "error",
+        "ignorePackages",
+        {
+          "js": "never",
+          "jsx": "never",
+          "ts": "never",
+          "tsx": "never"
+        }
+     ]
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
+    react: {
+        version: "detect",
+    },
+  },
+};
