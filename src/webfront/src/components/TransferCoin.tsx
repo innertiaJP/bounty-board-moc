98606968c649a69fa9689459e084e5675a96336e
innertiaJP <nft.toushi@gmail.com>
2022-03-29 20:58:59 +0900

    set route

---
+++ src/webfront/src/components/TransferCoin.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Button } from "@chakra-ui/react";
+
+function TransferCoin() {
+  return (
+    <>
+      <h1>Hello</h1>
+      <h2>Start</h2>
+    </>
+  );
+}
+
+export default TransferCoin;
